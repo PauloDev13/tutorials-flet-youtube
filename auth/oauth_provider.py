d5fe81696d6aeb998a29f7e4750f0e804f31f1e5
Paulo Roberto De Morais <prmorais1302@gmail.com>
2024-10-02 13:20:17 -0300

    Implementing authentication example

--- auth/oauth_provider.py
+++ auth/oauth_provider.py
@@ -8,18 +8,41 @@ def main(page: ft.Page) -> None:
     provider = GitHubOAuthProvider(
         client_id=os.getenv('GITHUB_CLIENT_ID'),
         client_secret=os.getenv('GITHUB_CLIENT_SECRET'),
-        redirect_url='http://localhost:8550/api/oauth/redirect',
+        redirect_url='http://localhost:8550/oauth/redirect',
     )
 
-    def login_click(e):
-        page.login(provider)
+    def login_button_click(e) -> None:
+        page.login(provider, scope=['public_repo'])
 
-    def on_login(e):
-        print('Access token:', page.auth.token.access_token)
-        print('User ID:', page.auth.user.id)
+    def on_login(e: ft.LoginEvent):
+        if not e.error:
+            toggle_login_buttons()
 
+    def logout_button_click(e) -> None:
+        page.logout()
+
+    def on_logout(e):
+        toggle_login_buttons()
+
+    def toggle_login_buttons() -> None:
+        login_button.visible = page.auth is None
+        logout_button.visible = page.logout is None
+        page.update()
+
+    login_button = ft.ElevatedButton(
+        text='Login com GitHub',
+        on_click=login_button_click,
+    )
+
+    logout_button = ft.ElevatedButton(
+        text='Logout',
+        on_click=logout_button_click,
+    )
+
+    toggle_login_buttons()
     page.on_login = on_login
-    page.add(ft.ElevatedButton(text='Login com GitHub', on_click=login_click))
+    page.on_logout = on_logout
+    page.add(login_button, logout_button)
 
 
 ft.app(target=main, port=8550, view=ft.AppView.WEB_BROWSER)
